24d65631ae38c2feee71e898291f9137763867ec
Reyad <20869625+AReyad@users.noreply.github.com>
2025-11-02 22:32:10 +0200

    Add player guess and validation

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+class Player
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def guess
+    user_input = gets.chomp
+    if valid_guess?(user_input)
+      user_input
+    else
+      puts 'Invalid guess, your guess should consist of one letter of the English Alphabet.'
+    end
+  end
+
+  def valid_guess?(input)
+    input.match?(/\A[a-zA-Z'-]*\z/) && input.length == 1
+  end
+end
