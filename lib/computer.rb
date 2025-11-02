86200fd433a62624a03dc6b479b25d176ef701a5
Reyad <20869625+AReyad@users.noreply.github.com>
2025-11-02 22:41:08 +0200

    Change max word length in dictionary filter

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,6 +1,9 @@
 class Computer
   attr_reader :word
 
+  MIN_WORD_LENGTH = 5
+  MAX_WORD_LENGTH = 12
+
   def initialize
     @word = generate_random_word
   end
@@ -9,7 +12,7 @@ class Computer
     dictionary = File.open('dictionary.txt', 'r')
 
     filtered_dictionary = dictionary.select do |word|
-      word if word.chomp.length.between?(5, 10)
+      word if word.chomp.length.between?(MIN_WORD_LENGTH, MAX_WORD_LENGTH)
     end
     dictionary.close
     filtered_dictionary
